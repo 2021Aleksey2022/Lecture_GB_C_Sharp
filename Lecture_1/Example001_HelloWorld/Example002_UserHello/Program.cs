929e0006ec5b5176ac2333e25224098637ff49c6
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 18:18:47 +1000

    Приветствие с пользователем

---
+++ Lecture_1/Example001_HelloWorld/Example002_UserHello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example002_UserHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string userName = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.Write(userName);
+            Console.ReadLine();
+        }
+    }
+}

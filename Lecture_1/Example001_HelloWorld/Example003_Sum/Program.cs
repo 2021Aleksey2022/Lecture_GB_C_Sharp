a5bc5a1fede953fcd9dcacf46d14e3b4cde9f03e
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 19:00:13 +1000

    Сумма двух чисел

---
+++ Lecture_1/Example001_HelloWorld/Example003_Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example003_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 23;
+            int numberB = 78;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}

1345116e86f0d78a480d2c0ccad8f71168bdb2e3
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 19:50:21 +1000

    Находим наибольшее число, с помошью if

---
+++ Lecture_1/Example001_HelloWorld/Example006_Base/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example006_Base
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 6;
+            int d = 8;
+            int e = 4;
+            int max = a;
+            if (a > max) max = a;
+            if (b > max) max = b;
+            if (c > max) max = c;
+            if (d > max) max = d;
+            if (e > max) max = e;
+            Console.Write("max = ");
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}

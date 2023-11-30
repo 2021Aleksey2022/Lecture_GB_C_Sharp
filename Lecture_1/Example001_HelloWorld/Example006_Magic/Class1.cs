dfe8dc4fc242d6a2ff221c48eaae9b4eafabc625
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 20:44:28 +1000

    Цикл while

---
+++ Lecture_1/Example001_HelloWorld/Example006_Magic/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example006_Magic
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int xa = 20, ya = 1, xb = 1, yb = 30, xc = 40, yc = 30;
+            Console.SetCursorPosition(xa, ya);
+            Console.Write("+");
+            Console.SetCursorPosition(xb, yb);
+            Console.Write("+");
+            Console.SetCursorPosition(xc, yc);
+            Console.Write("+");
+
+            int x = xa, y = xb;
+            int count = 0;
+            while(count < 10000)
+            {
+                int what = new Random().Next(0, 3);
+                if(what == 0)
+                {
+                    x = (x + xa) / 2;
+                    y = (y + ya) / 2;
+                }
+                else if(what == 1)
+                {
+                    x = (x + xb) / 2;
+                    y = (y + yb) / 2;
+                }
+                else
+                {
+                    x = (x + xc) / 2;
+                    y = (y + yc) / 2;
+                }
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("+");
+                count++;
+            }
+            Console.ReadLine();
+        }
+    }
+}

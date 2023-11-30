2ea29d2832f283508b9a558e384d8a6681cc099e
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 19:05:40 +1000

    Тип переменной double

---
+++ Lecture_1/Example001_HelloWorld/Example004_Double/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example004_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = 12;
+            double numberB = 5;
+            double result = numberA / numberB;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}

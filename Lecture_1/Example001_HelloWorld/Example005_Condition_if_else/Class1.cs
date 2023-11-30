c697381cf3e3b0979f23290ffb9a622cf69b9444
Алескей Щербаков <manay.ru@mail.ru>
2023-11-30 19:36:43 +1000

    Ветление if, else и метод Tolower

---
+++ Lecture_1/Example001_HelloWorld/Example005_Condition_if_else/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example001_HelloWorld.Example005_Condition_if_else
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string userName = Console.ReadLine();
+            if(userName.ToLower() == "маша")
+            {
+                Console.WriteLine("Ура, это же Маша!");
+            }
+            else
+            {
+                Console.Write("Привет, ");
+                Console.WriteLine(userName);
+            }
+            Console.ReadLine();
+        }
+    }
+}

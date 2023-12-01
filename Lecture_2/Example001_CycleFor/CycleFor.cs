c0f441d322bc898b8ba6948df6ab995537872b45
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 20:40:01 +1000

    Цикл for

---
+++ Lecture_2/Example001_CycleFor/CycleFor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lecture_2.Example001_CycleFor
+{
+    class CycleFor
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = new Stopwatch();
+            clock.Start();
+            int[] arr = { 1, 3, 5, -2, 54, 98 };
+            int max = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i + 1] > max)
+                {
+                    max = arr[i + 1];
+                }
+            }            
+            Console.WriteLine($"{max}");
+            Console.WriteLine(clock.ElapsedMilliseconds);
+            clock.Stop();
+            Console.ReadLine();
+        }
+    }
+}
